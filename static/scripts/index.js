c36e5f692f77b88b7cc061b2914759121d841269
OGDemonZ <71907588+OGDemonZ@users.noreply.github.com>
2023-09-28 16:13:03 -0600

    replacing index.js with alternate code

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,25 +1,16 @@
-const form = document.querySelector("form");
-const input = document.querySelector("input");
+function search(thisUrl) {
+  window.navigator.serviceWorker.register('./sw.js', {
+      scope: __uv$config.prefix
+  }).then(() => {
+      let url = thisUrl;
+      if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+      else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
-function search(site) {
-  window.navigator.serviceWorker
-    .register("./sw.js", {
-      scope: __uv$config.prefix,
-    })
-    .then(() => {
-      let url = site.trim();
-      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
-      else if (!(url.startsWith("https://") || url.startsWith("http://")))
-        url = "http://" + url;
-      sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
-      location.href = "go";
-})};
+      window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+});
+};
 
-function isUrl(val = "") {
-  if (
-    /^http(s?):\/\//.test(val) ||
-    (val.includes(".") && val.substr(0, 1) !== " ")
-  )
-    return true;
+function isUrl(val = ''){
+  if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
   return false;
-}
+};
