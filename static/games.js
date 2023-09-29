c021e7c7d90c7d933b9043103bbdedff527c007b
OGDemonZ <71907588+OGDemonZ@users.noreply.github.com>
2023-09-28 22:04:32 -0600

    finally fixed

--- static/games.js
+++ static/games.js
@@ -1,4 +1,4 @@
-let games = {
+games = {
     "1v1.lol":"https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/ko1ov/to@1e8a949f89fcf2b110640c41a0705db01405d161/d7uf4.xml",
     "2048":"https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/r3tq6l1/to9d2if1dv@f245b5d104814d56632fdf3c5e7845c3a1633a94/k39xn5j/d8g3s.xml",
     "Among Us":"https://blank-among-us.glitch.me/",
