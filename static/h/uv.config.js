b201e4f67ea352dfb2ab41ad7f40e4de7a533126
OGDemonZ <71907588+OGDemonZ@users.noreply.github.com>
2023-09-28 21:08:37 -0600

    adding my site appearence

--- static/h/uv.config.js
+++ static/h/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/astronomy/',
+    prefix: '/search/',
     bare: '/outerspace/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
