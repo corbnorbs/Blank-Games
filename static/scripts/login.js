fc0e2e0d95b164da1875b9be0d155198b484c13b
OGDemonZ <71907588+OGDemonZ@users.noreply.github.com>
2023-09-30 23:07:14 -0600

    adding submit

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,6 +1,26 @@
-function login() {
-    var loginHolder = document.createElement("div");
-    loginHolder.id = "login-holder";
-    document.body.appendChild(loginHolder);
-    console.log("LOGIN SCREEN");
+function openLogin() {
+    document.getElementById("container").classList.add("blur");
+    document.getElementById("login-holder").style.display = "block";
+}
+
+function closeLogin() {
+    document.getElementById("container").classList.remove("blur");
+    document.getElementById("login-holder").style.display = "none";
+}
+
+async function submitLogin() {
+    console.log("submit");
+    var gmail = document.getElementById("gmail").innerHTML;
+    var password = document.getElementById("password").innerHTML;
+    const data = {gmail, password};
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body:  JSON.stringify(data)
+        }
+        const response = await fetch('https://blank-games-database.glitch.me/api', options);
+        const json = await response.json();
+        console.log(json);
 }
