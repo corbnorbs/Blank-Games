cac01e679b32ed73676ac9849a91a624a52a7e9f
OGDemonZ <71907588+OGDemonZ@users.noreply.github.com>
2023-10-01 20:54:02 -0600

    auto cookie checker

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,4 +1,7 @@
-var UUID = "";
+console.log(Cookies.get('UUID'));
+if(Cookies.get('UUID')) {
+  
+}
 
 function openLogin() {
     document.getElementById("container").setAttribute("data-blurred", "true");
@@ -11,6 +14,25 @@ function closeLogin() {
     document.getElementById("login-holder").style.visibility = "hidden";
     document.getElementById("login-holder").setAttribute("data-shown", "false");
 }
+
+async function Login() {
+  if(Cookies.get('UUID') != undefined) {
+    var reason = "check UUID";
+    var myUUID = Cookies.get('UUID');
+    const data = {reason, myUUID};
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:  JSON.stringify(data)
+    }
+    const response = await fetch('https://blank-games-database.glitch.me/api', options);
+    const json = await response.json();
+    console.log(json);
+  }
+}
+
 
 async function submitLogin() {
     console.log("submit");
